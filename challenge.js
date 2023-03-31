578152ed73c76c691b44105fc34b434aa14d1b3e
unknown <anelemnyandu3@gmail.com>
2023-03-31 08:39:13 +0200

    If statement challege

---
+++ challenge.js
@@ -0,0 +1,48 @@
+FREE_WARNING = 'Free shipping only applies to single customer orders'
+BANNED_WARNIN = 'Unfortunately we do not ship to your country of residence'
+NONE_SELECTED = '0'
+
+
+
+location = 'RSA'
+
+
+if (location = "RSA") {
+    let shipping = 400
+    let currency = 'R'
+    shipping === 400 && currency =='R'
+}
+if (locatin = "NK") { 
+    const WARNING = "sorry, we do not ship to Nk"
+    console.log(WARNING)
+}
+if (location = "NAM") {
+    shipping = 600
+} else shipping = 800 
+
+shoes = 300 * 1
+toys = 100 * 5
+shirts = 150 *0
+batteries = 35 * 2
+pens = 5 * 0 
+shipping = 400
+currency = "$"
+customers = null
+calcShipping = null
+
+
+if (shoes + batteries + pens + shirts > 1000 || currency, 60  ) {
+	if (location = "NAM" && customers < 2) {
+			if (location = "RSA")
+		    shipping = 0 || calcShipping
+		}
+	}
+
+
+if (shipping = 0 && customers !== 1) { console.log(WARNING) }
+currency = "R"
+location == 'NK'? console.log(WARNING) : console.log('price', currency, shoes + batteries + pens + shirts + shipping + toys)
+
+customers = null
+
+currency = null
